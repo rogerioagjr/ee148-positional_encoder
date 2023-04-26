ab30216a920879e8524272d16351509dd17eaaba
rogerioguimaraes <rogerio.agjunior@gmail.com>
2023-04-25 18:23:26 -0700

    changed default transformer to original from Attention is All You Need

--- positional_encoder/models.py
+++ positional_encoder/models.py
@@ -8,8 +8,8 @@ from typing import Type
 
 class TransformerModel(nn.Module):
     def __init__(self, text_encoder: Type[nn.Module], pos_encoder: Type[nn.Module], n_tokens: int,
-                 d_model: int = 200, nhead: int = 2, d_hid: int = 200, num_layers: int = 2,
-                 dropout: float = 0.2, init_range: float = 0.1):
+                 d_model: int = 512, nhead: int = 8, d_hid: int = 2048, num_layers: int = 6,
+                 dropout: float = 0.1, init_range: float = 0.1):
         super().__init__()
         self.model_type = 'Transformer'
         self.text_encoder = text_encoder(n_tokens, d_model, init_range)
